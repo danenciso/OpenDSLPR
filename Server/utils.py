a482b22f1297ca0f53c8ca639455ecf6738fa434
Daniel Enciso <danenciso@gmail.com>
2018-07-09 11:36:45 -0400

    Changed utils.py to automatically located server address. Removes need for manual entry. Additional changes includes Dockerfile that needs editing in running Docker container.

--- Server/utils.py
+++ Server/utils.py
@@ -6,7 +6,7 @@ class Config():
 		self.port = int(sys.argv[1])						   				#port to be connected
 		self.no_of_conn = int(sys.argv[4])             						#remove if don't want a limit on number of connected clients
 		self.clients_sockets_array = [0]*self.no_of_conn 					#a structure consisting of identities of clients
-
+		
 		#client specific may need to be pushed to another client specific class
 		self.ring_buffer = RingBuffer(int(sys.argv[2]))
 		self.open_alpr = Recognize(int(sys.argv[3]), "ca", os.getcwd())
@@ -16,7 +16,7 @@ class Config():
 		# Done with initialization, now bind server to port
 		context = zmq.Context()
 		self.receiver = context.socket(zmq.PULL)
-		self.receiver.bind("tcp://10.244.19.219:"+str(self.port))				#Tcp connection
+		self.receiver.bind("tcp://*:"+str(self.port))				#Tcp connection
 
 class RingBuffer():
 	def __init__(self, size):
